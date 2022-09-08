9b0208b2050a7ad3ef7295a991ad8160afcf0e49
Netanel Cohen <netanel153@gmail.com>
2022-09-08 16:32:13 +0300

    feat: creation time increased to 12 hours

--- src/common/functions/queue.ts
+++ src/common/functions/queue.ts
@@ -1,7 +1,10 @@
-import PgBoss from 'pg-boss';
+import PgBoss, { SendOptions } from 'pg-boss';
 
 async function addToQueue(boss: PgBoss, model: string, key: string): Promise<void> {
-  const jobId = await boss.send(model, { key });
+  const options: SendOptions = {
+    retentionHours: 10,
+  };
+  const jobId = await boss.send(model, { key }, options);
 
   console.log(`created job in queue ${model}: ${jobId != undefined ? jobId : ''} Key: ${key}`);
 
@@ -9,7 +12,11 @@ async function addToQueue(boss: PgBoss, model: string, key: string): Promise<voi
 }
 
 async function addSizeToQueue(boss: PgBoss, model: string, size: number): Promise<void> {
-  const jobId = await boss.send(`${model}-size`, { size });
+  const options: SendOptions = {
+    retentionHours: 10,
+  };
+
+  const jobId = await boss.send(`${model}-size`, { size }, options);
 
   console.log(`created job in queue ${model}: ${jobId != undefined ? jobId : ''}`);
 
